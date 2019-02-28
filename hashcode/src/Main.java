9d052590f6987131c8cbfa7a9b1781d9086749a4
Iman Radjavi <radjavi@hotmail.com>
2019-02-28 18:50:12 +0100

    ny

--- hashcode/src/Main.java
+++ hashcode/src/Main.java
@@ -9,41 +9,19 @@ public class Main {
         BufferedReader reader = new BufferedReader(new FileReader(inputFile));
         String firstLine = reader.readLine();
         String[] firstLineItems = firstLine.split(" ");
-        int rowCount = Integer.parseInt(firstLineItems[0]);
-        int colCount = Integer.parseInt(firstLineItems[1]);
-        int minIngredient = Integer.parseInt(firstLineItems[2]);
-        int maxArea = Integer.parseInt(firstLineItems[3]);
+        int photoCount = Integer.parseInt(firstLineItems[0]);
         // Pizza contains every row of the pizza as a String
-        List<String> pizza = new ArrayList<>();
-        for (int i=0; i<rowCount; i++) {
-            pizza.add(reader.readLine());
+        List<String> photos = new ArrayList<>();
+        for (int i=0; i<photoCount; i++) {
+            photos.add(reader.readLine());
         }
         reader.close();
 
         // Greedy solution from video 'Get Ready for Hash Code 2018'
         // Looks only horizontally
         List<String> result = new ArrayList<>();
-        for (int r=0; r<rowCount; r++) {
-            int beg = 0;
-            int end = 0;
-            int mushrooms = 0;
-            int tomatos = 0;
-            while (end < colCount) {
-                if (pizza.get(r).charAt(end) == 'M') mushrooms++;
-                else if (pizza.get(r).charAt(end) == 'T') tomatos++;
-                end++;
-                if (end - beg > maxArea) {
-                    if (pizza.get(r).charAt(beg) == 'M') mushrooms--;
-                    else if (pizza.get(r).charAt(beg) == 'T') tomatos--;
-                    beg++;
-                }
-                if (end - beg <= maxArea && mushrooms >= minIngredient && tomatos >= minIngredient) {
-                    result.add(r + " " + beg + " " + r + " " + (end-1));
-                    beg = end;
-                    tomatos = 0;
-                    mushrooms = 0;
-                }
-            }
+        for (int r=0; r<photoCount; r++) {
+            
         }
 
         // Create output file
